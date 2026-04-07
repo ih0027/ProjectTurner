f2822ca5e25b08e585808c9d09469ec9eb65fc25
ih0027 <119897082+ih0027@users.noreply.github.com>
2026-04-07 11:07:59 -0400

    Added sh files

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+#Start and enable pigpio service
 sudo pigpiod
 sudo systemctl enable pigpiod
 
+#Install required python libraries
+python3 -m pip install pigpio keyboard
---
+++ pageTurner.sh
@@ -0,0 +1,4 @@
+#TODO add initialization to connect to bluetooth device
+
+#Run script
+sudo python3 main.py
